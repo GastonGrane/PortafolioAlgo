119946c3274c5012d000c9c2fc5bf1fef4b253b2
Gaston <gaston.grane@correo.ucu.edu.uy>
2025-06-20 19:21:30 -0300

    PD11

---
+++ src/main/java/PDS/UT3/PD11/ParImpar.java
@@ -0,0 +1,27 @@
+package PDS.UT3.PD11;
+
+import TDAS.Lista;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        Lista<Integer> listaDesordenada =  new Lista<>();
+        listaDesordenada.insertar(4,4);
+        // 4, 71, 3, 6, 12, 8, 35, 11, 22, 17
+        listaDesordenada.insertar(71,71);
+        listaDesordenada.insertar(3,3);
+        listaDesordenada.insertar(6,6);
+        listaDesordenada.insertar(12,12);
+        listaDesordenada.insertar(8,8);
+        listaDesordenada.insertar(35,35);
+        listaDesordenada.insertar(11,11);
+        listaDesordenada.insertar(22,22);
+        listaDesordenada.insertar(17,17);
+
+        System.out.println(listaDesordenada.imprimir());
+
+        Lista<Integer> listaResultante = listaDesordenada.ordenarParImpar();
+
+        System.out.println(listaResultante.imprimir());
+
+    }
+}
